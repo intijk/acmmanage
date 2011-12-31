5fc237d464f5c4a0fd6f017987cf3bc0dd7c79b7
justzx <justzx2011@gmail.com>
2011-12-31 21:11:33 +0800

    修改了主页上链接的错误

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo <<<eot
 		<div align="left" style="margin-left:20%;margin-right:20%">
 		<ul>
 
-			<li>注册方法：将你在<a href="acm.xidian.edu.cn/land"/>西电Online Judge</a>的账号和姓名，学院，年级发给acmxidian@gmail.com. </li>
+			<li>注册方法：将你在<a href="http://acm.xidian.edu.cn/land"/>西电Online Judge</a>的账号和姓名，学院，年级发给acmxidian@gmail.com. </li>
 			<br/>
 			<li>当你的账号审核通过以后，就可以用西电Online Judge的账号登录此系统.</li>
 			<br/>
