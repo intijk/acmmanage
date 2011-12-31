102bd7afaa579776f5f176aab38e3159727c0d73
justzx <justzx2011@gmail.com>
2011-12-31 19:26:42 +0800

    更新了usaco页面的呈现

--- usaco.php
+++ usaco.php
@@ -4,17 +4,21 @@ require_once("conf.php");
 require_once("f.php");
 ?>
 	<meta charset='utf-8'/>
-<link rel="stylesheet" type="text/css" href="ustyle.css"/>
+<link rel="stylesheet" type="text/css" href="u.css"/>
 	<title>
 		西电ACM 寒假USACO刷题大比拼!
 	</title>
 	<head>
-		<h1>西电ACM 寒假USACO刷题大比拼!</h1>
+		<h1>寒假 <a href="http://ace.delos.com/usacogate"><img class="ver2" src="./usaco2.png"/></a> 刷题 <img class="ver1" src="./dabipin2.png"/></h1>
 	</head>
 	<body>
-	&nbsp; &nbsp; &nbsp; 寒假放假期间，有没有想要提高自己的算法功底，训练自己的代码能力呢？ 参与到西电ACM寒假USACO刷题大比拼中来吧！这里有高手指导，菜鸟陪练，保证你寒假独自刷题不再寂寞，还在等什么呢？</br></br>
- 	&nbsp; &nbsp; &nbsp; 发送 XDU OJ 的 ID 到 acmxidian@gmail.com 开通，<a href="http://acm.xidian.edu.cn/acmmanage">填写 USACO 帐号</a>进入刷题榜哦！<br/>
+ 	<h2>发送 XDU OJ 的 ID 到 acmxidian@gmail.com 开通，<a href="http://acm.xidian.edu.cn/acmmanage">填写 USACO 帐号</a>进入刷题榜！</h2><br/>
 <br/>
+<table class="nav"><tr>
+<td><a href="http://acm.xidian.edu.cn/land">XDOJ</a></td>
+<td><a href="http://acm.xidian.edu.cn/acmmanage">训练</a></td>
+<td><a href="http://ace.delos.com/usacogate">USACO</td>
+</table></tr>
 <?php
 $link=linkToDBandSelectDB($db_name1);
 $sql="select t.username as username ,p.nickname as nickname ,t.updateTime as updateTime,t.value as value from RecentTrainingQuery as t,personinfo as p where ojType='usaco' and t.username=p.username order by t.value desc,t.updateTime asc";
@@ -33,7 +37,7 @@ while($r=mysql_fetch_assoc($result)){
 		echo "<tr class='alt'>";
 	}
 	echo '<td>';
-	echo '第' . $i  . '名';
+	echo '第 ' . $i  . ' 名';
 	echo '</td>';
 	echo '<td>';	
 	echo $r['username'];
@@ -48,7 +52,7 @@ while($r=mysql_fetch_assoc($result)){
 	echo '<td>';
 	$score=(int)($r['value']/10) . "." . (int)($r['value']%10);
 	if($score=='6.2'){
-		echo "<img src='finish.png' width='140em'></img>";
+		echo "<img src='finish.png' width=120px></img>";
 	}else{
 		echo $score;
 	}
@@ -63,5 +67,6 @@ echo <<<eot
 </table>
 eot;
 ?>
+
 	</body>
 </html>
